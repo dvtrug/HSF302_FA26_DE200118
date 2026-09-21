6a02f21dfb81ded3a15cf6f370024a7226808951
Do Trung <thanhtrungxxz@gmail.com>
2026-09-21 19:36:26 +0700

    feat(TODO 5.8): add JPQL count active employee and get salary

---
+++ Slot4/Chapter1_JPA_MappingManyToMany/src/main/java/fu/de200118/dao/ProjectDAO.java
@@ -0,0 +1,30 @@
+package fu.de200118.dao;
+
+import fu.de200118.pojo.Project;
+import fu.de200118.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class ProjectDAO {
+    private final EntityManagerFactory emf = JPAUtil.getEMF();
+
+    public void countActiveEmployeeInProjectAndGetSumSalary(Project project) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createQuery(
+                    "SELECT p.projectName, COUNT(e), SUM(e.salary)\n" +
+                    "FROM Project p JOIN p.employees e\n" +
+                    "WHERE e.active = true\n" +
+                    "GROUP BY p.projectName\n");
+
+            em.merge(project);
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+}
